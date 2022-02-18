956617837993fbf602a73ad01bac0bd58a878374
Akshit Sethi <akshitsethi@gmail.com>
2022-02-18 12:05:21 +0530

    Cleanup

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 /**
  * File which gets called on plugin uninstall. Since the plugin does not do any
  * sort of setup, nothing is done over here.
- * 
+ *
  * Ideally, this file is used to cleanup plugin stuff from the database, etc.
  *
  * @package AwesomeMotive\AMPlugin
